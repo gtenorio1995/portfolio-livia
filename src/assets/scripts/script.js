6ef8dccb0abbc2be31da706f2baa0a1370f529b5
Gabriel Tenório de Oliveira <gabrieltenorio17@gmail.com>
2024-05-12 01:14:24 -0300

    build: add scrip.js for navigation links

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,9 +1,27 @@
 const btnMenu = document.querySelector("#menu-burguer");
 const menuList = document.querySelector("#menu-list");
+const arrayMenu = document.querySelectorAll("#menu-list li");
 
 const openMenu = () => {
   menuList.classList.toggle("openMenu");
   btnMenu.classList.toggle("active");
 };
+
+function selectedLink() {
+  arrayMenu.forEach((item) => {
+    item.classList.remove("selected");
+  });
+  this.classList.add("selected");
+  menuList.classList.toggle("openMenu");
+  btnMenu.classList.toggle("active");
+}
+
+arrayMenu.forEach((item) => {
+  item.addEventListener("click", selectedLink);
+});
+
+arrayMenu.forEach((item) => {
+  item.addEventListener("touch", selectedLink);
+});
 
 btnMenu.addEventListener("click", openMenu);
